056df51389642f2d2ab41f38e3937a6bab69f813
harris <khanharris7@yahoo.com>
2021-07-06 19:21:35 -0500

    added eventlistner

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
-//https://api.frankfurter.app/currencies
+const dogResult = document.getElementById('#dog_result')
+const dog_pic_butn = document.getElementById('#dog_pic_butn');
+const LikeButton = document.getElementById('#like_butn');
 
+dog_pic_butn.addEventListener(click, getRandomDog)
+
+
+function getRandomDog() {
+    fetch('https://random.dog/woof.json')
+        .then(res => res.json())
+        .then(data => {
+            if(data.url.includes('.mp4')) {
+                getRandomDog()
+            } else {
+
+                dogResult.innerHTML = `img src="${data.url}"/>` 
+            }
+            
+        })
+}
